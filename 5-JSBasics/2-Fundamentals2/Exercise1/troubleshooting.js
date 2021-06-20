7718ced79d536f25d3161563bb4b1376d16aa7d6
Mark P <mj.poso18@gmail.com>
2021-06-20 21:35:15 +0800

    JS "Fundamentals 2" Exercise 1

---
+++ 5-JSBasics/2-Fundamentals2/Exercise1/troubleshooting.js
@@ -0,0 +1,22 @@
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not,
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+ */
+
+function troubleshooting() {
+  const a = 1;
+  const b = 1;
+
+  // Edit between these lines
+  // =================================
+
+  // return 'a' + 'b';  // These are concatenating strings
+  return a + b;
+
+  // =================================
+}
+
+// Do not change this
+module.exports = troubleshooting;
