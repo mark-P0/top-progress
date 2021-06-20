a592728cce1c1a2c8adeeafc3e50e184ffd09e06
Mark P <mj.poso18@gmail.com>
2021-06-21 07:53:36 +0800

    "Fundamentals 4" Finished `repeatString` exercise

--- 5-JSBasics/9-Fundamentals4/2-repeatString/repeatString.js
+++ 5-JSBasics/9-Fundamentals4/2-repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, count) {
+  // let test = Array.from({ length: count }).fill(string);
+  // console.log(test.reduce((running, current) => running + current));
 
+  if (count < 0) return 'ERROR';
+
+  return Array.from({ length: count })
+    .fill(string)
+    .reduce((running, current) => running + current, '');
 };
 
 module.exports = repeatString;
