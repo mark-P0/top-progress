c05eb7da454e03e41c4e8bf518bd40a0444a9d78
Mark P <mj.poso18@gmail.com>
2021-06-21 09:02:25 +0800

    "Fundamentals 4" Finished `tempConversion` exer.

--- 5-JSBasics/9-Fundamentals4/7-tempConversion/tempConversion.js
+++ 5-JSBasics/9-Fundamentals4/7-tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  return roundToDecimalPlaces((fahrenheit - 32) * (5 / 9), 1);
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  return roundToDecimalPlaces(celsius * (9 / 5) + 32, 1);
 };
+
+function roundToDecimalPlaces(number, places) {
+  return Math.round(number * 10 ** places) / 10 ** places;
+}
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
