54c71045ba7942ca7c96eeedc08b50a09a6a789d
Mark P <mj.poso18@gmail.com>
2021-06-21 08:54:10 +0800

    "Fundamentals 4" Finished `leapYears` exercise

--- 5-JSBasics/9-Fundamentals4/6-leapYears/leapYears.js
+++ 5-JSBasics/9-Fundamentals4/6-leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      // if (year % 400 == 0) return true;
+      // return false;
+      return year % 400 == 0;
+    }
+    return true;
+  }
+  return false;
 };
+
+for (year of [1984, 2004, 1800, 1900, 1600, 2000, 1985]) {
+  console.log(leapYears(year));
+}
 
 module.exports = leapYears;
