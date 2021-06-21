9f89d2313f8af6d5eae3e6e9c344455c855509f8
Mark P <mj.poso18@gmail.com>
2021-06-21 08:37:03 +0800

    "Fundamentals 4" Finished `sumAll` exercise

--- 5-JSBasics/9-Fundamentals4/5-sumAll/sumAll.js
+++ 5-JSBasics/9-Fundamentals4/5-sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (lowerLimit, upperLimit) {
+  for (element of [lowerLimit, upperLimit]) {
+    if (!(typeof element === 'number') || element < 0) return 'ERROR';
+  }
 
+  if (lowerLimit > upperLimit)
+    [lowerLimit, upperLimit] = [upperLimit, lowerLimit];
+
+  return Array.from(
+    { length: upperLimit - lowerLimit + 1 },
+    (___, idx) => idx + lowerLimit
+  ).reduce((running, current) => running + current);
 };
+
+// console.log(sumAll(1, 4));
+// console.log(sumAll(1, '4'));
 
 module.exports = sumAll;
