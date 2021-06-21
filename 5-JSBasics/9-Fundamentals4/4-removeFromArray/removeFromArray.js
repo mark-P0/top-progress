377d168edd44f6526f21e14071d00185257031fc
Mark P <mj.poso18@gmail.com>
2021-06-21 08:11:03 +0800

    "Fundamentals 4" Finished `removeFromArray` exer.

--- 5-JSBasics/9-Fundamentals4/4-removeFromArray/removeFromArray.js
+++ 5-JSBasics/9-Fundamentals4/4-removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let [givenArray, ...elementsToRemove] = arguments;
+  return givenArray.filter(
+    (element, ___, ____) => !elementsToRemove.includes(element)
+  );
 
+  // return array.filter((element, ___, ____) => element !== toRemove);
 };
 
 module.exports = removeFromArray;
