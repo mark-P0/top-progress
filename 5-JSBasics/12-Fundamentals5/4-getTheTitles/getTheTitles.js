1c0f8a134cc2f4cce56991a18544c7b9630ab821
Mark P <mj.poso18@gmail.com>
2021-06-23 04:01:43 +0800

    "Fundamentals 5" `getTheTitles` finished

--- 5-JSBasics/12-Fundamentals5/4-getTheTitles/getTheTitles.js
+++ 5-JSBasics/12-Fundamentals5/4-getTheTitles/getTheTitles.js
@@ -1,5 +1,40 @@
-const getTheTitles = function() {
+const getTheTitles = function (booksList) {
+  /* book = {title: string, author: string} */
 
+  // return booksList.reduce(
+  //   (runningList, book) => (runningList = [...runningList, book.title]),
+  //   []
+  // );
+
+  return booksList.reduce((list, { title }) => (list = [...list, title]), []);
 };
+// console.log(
+//   getTheTitles([
+//     {
+//       title: 'Book',
+//       author: 'Name',
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2',
+//     },
+//   ])
+// );
+
+const getTheTitlesMap = function (booksList) {
+  return booksList.map(({ title }) => title);
+};
+// console.log(
+//   getTheTitlesMap([
+//     {
+//       title: 'Book',
+//       author: 'Name',
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2',
+//     },
+//   ])
+// );
 
 module.exports = getTheTitles;
