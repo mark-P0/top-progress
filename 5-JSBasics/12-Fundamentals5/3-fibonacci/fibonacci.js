abb120c41b748c721e2a3a8dc7b191f412abcc0d
Mark P <mj.poso18@gmail.com>
2021-06-23 04:00:26 +0800

    "Fundamentals 5" Used `fibonacci` test cases

--- 5-JSBasics/12-Fundamentals5/3-fibonacci/fibonacci.js
+++ 5-JSBasics/12-Fundamentals5/3-fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = function (num) {
+  num = parseInt(num) || -1;
+
+  if (num === 0) return 0;
+  if (num < 0) return 'OOPS';
+
   let series = [1, 1];
+  if (num === 1) return series[0];
 
   for (___ of Array(num - 2)) {
     series = [series[1], series.reduce((sum, item) => (sum += item))];
@@ -8,5 +14,10 @@ const fibonacci = function (num) {
   return series[1];
 };
 // console.log(fibonacci(6));
+// console.log(fibonacci(0));
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(4));
 
 module.exports = fibonacci;
