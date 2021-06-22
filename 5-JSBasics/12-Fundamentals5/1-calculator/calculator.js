e30d946f3f92becadda9adc0168f35b31b46064e
Mark P <mj.poso18@gmail.com>
2021-06-23 01:57:04 +0800

    "Fundamentals 5" `calculator` finished

--- 5-JSBasics/12-Fundamentals5/1-calculator/calculator.js
+++ 5-JSBasics/12-Fundamentals5/1-calculator/calculator.js
@@ -1,26 +1,87 @@
-const add = function() {
-	
+function print(anything) {
+  if (
+    (typeof require !== 'undefined' && require.main === module) ||
+    require.main.filename.includes('quokka')
+  )
+    console.log(anything);
+}
+
+const add = function (a, b) {
+  return a + b;
+};
+print(add(2, 3));
+
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const subtract = function() {
-	
+const sum = function (sequence) {
+  return sequence.reduce((running, element) => (running += element), 0);
+};
+print(sum([2, 3, 4, 5, 6]));
+print(sum([1]));
+print(sum([0]));
+print(sum([]));
+print(sum([-1, -2, -3]));
+
+/* `arguments` approach */
+const sumArguments = function () {
+  return Array.from(arguments).reduce((run, arg) => {
+    if (typeof arg === 'object') return (run += sumArguments(...arg));
+    return (run += parseFloat(arg) || 0);
+  }, 0);
+};
+print(sumArguments(1, 2, 3, [4, 5], 'haha'));
+
+/* Spread parameter approach */
+const sumSpread = function (...args) {
+  return args.reduce((run, arg) => {
+    if (typeof arg === 'object') return (run += sumSpread(...arg));
+    return (run += parseFloat(arg) || 0);
+  }, 0);
+};
+print(sumSpread(6, 2, 3, [4, 5], 'haha'));
+print(sumSpread());
+print(sumSpread(0));
+print(sumSpread(1));
+print(sumSpread(NaN));
+print(sumSpread(undefined));
+
+const multiply = function (sequence) {
+  if (!sequence.length) return 0;
+  return sequence.reduce((running, element) => (running *= element), 1);
+};
+print(multiply([2, 3, 4, 5, 6]));
+print(multiply([1]));
+print(multiply([0]));
+print(multiply([]));
+print(multiply([-1, -2, -3]));
+
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const sum = function() {
-	
-};
+const factorial = function (num) {
+  // return Array.from({ length: num }, (___, idx) => idx)
 
-const multiply = function() {
+  // return Array(num)
+  //   .keys()
+  //   .forEach((x, i) => {
+  //     print(x, i);
+  //   });
 
-};
+  if (num < 0) return;
 
-const power = function() {
-	
-};
+  num = Math.max(num, 1);
 
-const factorial = function() {
-	
+  return Array.from(Array(num).keys()).reduce((run, idx) => run * (idx + 1), 1);
 };
+print(factorial(10));
+print(factorial(3));
+print(factorial(2));
+print(factorial(1));
+print(factorial(0));
+print(factorial(-1));
 
 module.exports = {
   add,
@@ -28,5 +89,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
