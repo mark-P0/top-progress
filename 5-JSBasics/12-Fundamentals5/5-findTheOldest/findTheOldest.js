751a3441dacf7aa3a7ccaf879062187cbf1dcc63
Mark P <mj.poso18@gmail.com>
2021-06-23 04:13:16 +0800

    "Fundamentals 5" `findTheOldest` finished

--- 5-JSBasics/12-Fundamentals5/5-findTheOldest/findTheOldest.js
+++ 5-JSBasics/12-Fundamentals5/5-findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  /* person = {name: string, yearOfBirth: number, yearOfDeath?: number} */
+  let currentYear = new Date().getFullYear();
+  return people.reduce((oldest, person) => {
+    return (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth >
+      (person.yearOfDeath || currentYear) - person.yearOfBirth
+      ? oldest
+      : person;
+  });
 };
+// console.log(new Date().getFullYear());
 
 module.exports = findTheOldest;
