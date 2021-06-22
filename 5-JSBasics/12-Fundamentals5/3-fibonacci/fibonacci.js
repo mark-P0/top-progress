afb5369c8798ce1709219dfb242b85bddeac306c
Mark P <mj.poso18@gmail.com>
2021-06-23 03:45:23 +0800

    "Fundamentals 5" `fibonacci` finished

--- 5-JSBasics/12-Fundamentals5/3-fibonacci/fibonacci.js
+++ 5-JSBasics/12-Fundamentals5/3-fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let series = [1, 1];
 
+  for (___ of Array(num - 2)) {
+    series = [series[1], series.reduce((sum, item) => (sum += item))];
+  }
+
+  return series[1];
 };
+// console.log(fibonacci(6));
 
 module.exports = fibonacci;
